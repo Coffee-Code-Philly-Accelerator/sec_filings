bbca08dc58cd29b1b37f381b0231dde4e80ad600
Tony363 <pysolver@gmail.com>
2023-08-26 00:24:37 -0400

    just need to pick out sub headers

--- consolidate_tables.py
+++ consolidate_tables.py
@@ -5,6 +5,7 @@ import glob
 import pandas as pd
 import numpy as np
 from collections import Counter
+from functools import reduce
 from fuzzywuzzy import process
 
 from scrap_links import init_logger,arguements
@@ -57,22 +58,31 @@ def debug_format(
 def clean(
     file:str,
 )->list:
-    if 'main' in file:
-        return
     dirs = file.split('/')
     if  len(dirs) < 3 or '.csv' not in dirs[-1]:
         return
     df_cur = pd.read_csv(file)
     df_cur = df_cur.T.drop_duplicates().T
-    df_cur.dropna(axis=1,thresh=7,inplace=True) # allowable nan threshold
+    # df_cur.dropna(axis=1,thresh=7,inplace=True) # allowable nan threshold
     df_cur = df_cur.iloc[1:,1:]
     if df_cur.shape[1] < 4:
         return
-    columns_to_drop = df_cur.columns[df_cur.iloc[0].isna()]
-    df_cur = df_cur.drop(columns=columns_to_drop)
+    # columns_to_drop = df_cur.columns[df_cur.iloc[0].isna()]
+    # df_cur = df_cur.drop(columns=columns_to_drop)
     df_cur = df_cur.dropna(how='all')
     if df_cur.empty:
         return
+    
+    df_cur.reset_index(drop=True,inplace=True)
+    important_fields,idx = get_key_fields(df_cur)
+    df_cur.columns = important_fields
+    df_cur['date'] = dirs[1]
+    df_cur = merge_duplicate_columns(df_cur)
+    
+    cur_cols,standard_names = df_cur.columns.tolist(),standard_field_names()
+    cols_to_drop = [col for col in cur_cols if col not in standard_names] 
+    df_cur.drop(columns=cols_to_drop, errors='ignore',inplace=True)
+    df_cur.drop(index=idx,inplace=True)
     return df_cur
 
 def present_substrings(substrings, main_string):
@@ -120,32 +130,18 @@ def process_date(
     dfs = {}
     for file in os.listdir(os.path.join('csv',date)):
         df_cur = clean(os.path.join('csv',date,file))
-        if df_cur is None:
+        if df_cur is None or df_cur.empty:
             continue
-        
-        df_cur.reset_index(drop=True,inplace=True)
-        important_fields,idx = get_key_fields(df_cur)
-        df_cur.columns = important_fields
-        df_cur['date'] = date
-        df_cur = merge_duplicate_columns(df_cur)
-        
-        cur_cols,standard_names = df_cur.columns.tolist(),standard_field_names()
-        cols_to_drop = [col for col in cur_cols if col not in standard_names] \
-            if any(col in standard_names for col in cur_cols) else []
-        df_cur.drop(columns=cols_to_drop, errors='ignore',inplace=True)
-        
         key = '_'.join(tuple(map(str,df_cur.columns.tolist()))).replace('/','_')
         key = key.replace(' ','_')
         if dfs.get(key) is None:
             dfs[key] = []
-
-        df_cur.drop(index=idx,inplace=True)
         dfs[key].append(df_cur)
      
     if not os.path.exists(f"csv/{date}/output"):
         os.mkdir(f"csv/{date}/output")   
     for t in dfs:
-        if os.path.exists(f"csv/{date}/output/{t}.csv") or len(t) > 100:
+        if os.path.exists(f"csv/{date}/output/{t}.csv") or len(t.split('_')) < 6:
             continue
         result = pd.concat(dfs[t], axis=0,join='outer', ignore_index=True)
         result.to_csv(f"csv/{date}/output/{t}.csv")
@@ -154,59 +150,22 @@ def merge_duplicate_columns(
     df:pd.DataFrame,
 )->pd.DataFrame:
     duplicate_cols = df.columns[df.columns.duplicated(keep=False)]
-    # Merge columns with the same name
     for col_name in duplicate_cols.unique():
-        # Select columns with the same name
         duplicate_data = df.loc[:, df.columns == col_name]
-        # Concatenate the values in these columns row-wise and store in a new 
-        # logging.debug(f"{duplicate_data[0,:].dropna().astype(str)} {len(duplicate_data[0].dropna().astype(str))}")
         merged_data = duplicate_data.apply(lambda row: ' '.join(set(row.dropna().astype(str))), axis=1)
-        # Drop the original duplicate columns
         df = df.loc[:, df.columns != col_name]
-        # Add the new merged column
         df[col_name] = merged_data
     return df
 
 
 def join_all_possible()->None:
-    infile = 'csv/**/*/*'
+    infile = 'csv/*/output/*'
     all_csvs = glob.glob(infile,recursive=True)
-    dfs = {}
-    
-    for file in all_csvs:
-        dirs = file.split('/')
-        df_cur = clean(file)
-        if df_cur is None:
-            continue
-        
-        df_cur.reset_index(drop=True,inplace=True)
-        important_fields,idx = get_key_fields(df_cur)
-        df_cur.columns = important_fields
-        df_cur['date'] = dirs[1]
-        df_cur.drop(df_cur.columns[-2],axis=1,inplace=True)
-        
-        logging.debug(f"DUPLICATED COLUMNS - {df_cur.columns[df_cur.columns.duplicated(keep=False)]}")
-        df_cur = merge_duplicate_columns(df_cur)
-        
-        cur_cols,standard_names = df_cur.columns.tolist(),standard_field_names()
-        cols_to_drop = [col for col in cur_cols if col not in standard_names] \
-            if any(col in standard_names for col in cur_cols) else []
-        df_cur.drop(columns=cols_to_drop, errors='ignore',inplace=True)
-        
-        key = '_'.join(tuple(map(str,df_cur.columns.tolist()))).replace('/','_')
-        key = key.replace(' ','_')
-        if dfs.get(key) is None:
-            dfs[key] = []
-
-        df_cur.drop(index=idx,inplace=True)
-        dfs[key].append(df_cur)
-        
-    for t in dfs:
-        if os.path.exists(f"csv/{t}.csv") or len(t) > 100:
-            continue
-        result = pd.concat(dfs[t], axis=0,join='outer', ignore_index=True)
-        result.to_csv(f"csv/{t}.csv")
-                
+    dfs = [pd.read_csv(csv) for csv in all_csvs]
+    merged_df = pd.concat(dfs)
+    merged_df.drop(columns=merged_df.columns[0],inplace=True)
+    logging.debug(f"final table shape - {merged_df.shape}")
+    merged_df.to_csv('csv/soi_table_all_possible_merges.csv')    
     return
 
 def main()->None:
@@ -222,13 +181,6 @@ def main()->None:
     return 
 
 if __name__ == "__main__":
-    """
-    See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
-    df[col_name] = merged_data
-    consolidate_tables.py:160: SettingWithCopyWarning:
-    A value is trying to be set on a copy of a slice from a DataFrame.
-    Try using .loc[row_indexer,col_indexer] = value instead
-    """
     # remove files that don't contain keyword
     # https://unix.stackexchange.com/questions/150624/remove-all-files-without-a-keyword-in-the-filename 
     # https://stackoverflow.com/questions/26616003/shopt-command-not-found-in-bashrc-after-shell-updation
