30a64c9c092580c01883d0e8f2fdc6ab5c859298
Tony <thefool363@gmail.com>
2024-11-11 02:46:09 -0500

    refactoring ciks

--- 1675033/extract_tables.py
+++ 1675033/extract_tables.py
@@ -149,7 +149,7 @@ def main() -> None:
 
     for i in range(urls.shape[0]):
         _, table_date, url = urls.iloc[i]
-        # table_date, url = '2018-12-31', 'https://www.sec.gov/Archives/edgar/data/0001512931/000114420419012276/tv514438_10k.htm'
+        # table_date, url = '2024-09-30', 'https://www.sec.gov/ix?doc=/Archives/edgar/data/0001675033/000095017024119231/gecc-20240930.htm'
         logger.info(f"DATETIMES - {table_date}")
         if len(glob.glob(os.path.join(args.cik,table_date,"*.csv"))) > 0:
             continue
